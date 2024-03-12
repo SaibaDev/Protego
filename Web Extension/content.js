086874b0fc5eca7716293ce40864986abcf86335
Syber Blaise Tolledo <Syber.tolledo@gmail.com>
2024-03-12 12:48:09 +0800

    qweqeqwe

--- Web Extension/content.js
+++ Web Extension/content.js
@@ -1,6 +1,8 @@
-document.addEventListener('click', (e) => {
-    const url = e.target.href;
+document.addEventListener('click', function(e) {
+    var url = e.target.href;
     if (url) {
         chrome.runtime.sendMessage({action: "captureUrl", url: url});
+        console.log('URL : "' + url + '"');
+
     }
 });
--- Web Extension/popup.js
+++ Web Extension/popup.js
@@ -1,42 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('toggleCapture');
-   // const predictionDisplay = document.getElementById('prediction');
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.getElementById('toggleCapture');
+   // var predictionDisplay = document.getElementById('prediction');
 
-    chrome.storage.local.get('captureEnabled', (data) => {
-        const captureEnabled = data.captureEnabled || false;
+    chrome.storage.local.get('captureEnabled', function(data) {
+        var captureEnabled = data.captureEnabled || false;
         updateToggleButton(captureEnabled);
     });
 
-    toggleButton.addEventListener('click', () => {
-        chrome.storage.local.get('captureEnabled', (data) => {
-            const captureEnabled = !data.captureEnabled;
+    toggleButton.addEventListener('click', function() {
+        chrome.storage.local.get('captureEnabled', function(data) {
+            var captureEnabled = !data.captureEnabled;
             chrome.storage.local.set({'captureEnabled': captureEnabled});
             updateToggleButton(captureEnabled);
         });
     });
+    
     function updateToggleButton(captureEnabled) {
-        toggleButton.innerText = captureEnabled ? 'Deactivate Capture' : 'Activate Capture';
         if (captureEnabled) {
+            toggleButton.innerText = 'Deactivate Capture';
             startCapture();
         } else {
+            toggleButton.innerText = 'Activate Capture';
             stopCapture();
         }
     }
+    
     function startCapture() {
         chrome.runtime.sendMessage({action: "startCapture"});
         toggleButton.style.backgroundColor='#FF0000';
         toggleButton.style.transition='.5s ease';
 
     }
+    
     function stopCapture() {
         chrome.runtime.sendMessage({action: "stopCapture"});
         toggleButton.style.backgroundColor='#008000';
         toggleButton.style.transition='.5s ease';
     }
-    /*chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    /*chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         if (message.action === "prediction") {
-            const prediction = message.prediction;
-            predictionDisplay.innerText = `Prediction: ${prediction}`;
+            var prediction = message.prediction;
+            predictionDisplay.innerText = 'Prediction: ' + prediction;
         }
     });*/
 });
